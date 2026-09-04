b5532b9712bd07037b6fcb2b35316d56bf0d7a6a
kathia_guzman <kmartinez70@ucol.mx>
2026-09-04 09:51:24 -0600

    Initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("======================\n");
+    printf("!hola git hut¡ \n");
+    printf("=====================\n");
+    printf("mi nombre es: kathia yukari martinez guzman");
+    printf("estoy aprendiendo programacion\n");
+    printf("este es mi primer programa\n");
+    printf("publicado en git hub\n");
+    printf("bienvenido a mi repositorio\n");
+    return 0;
+}
+
+
+
